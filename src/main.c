5105b1c2d11ab4f28d1a437fc78aeb07b5c66bb5
Alexandre Kubiaczyk <alexandre.kubiaczyk@epitech.eu>
2026-03-19 13:21:29 +0100

    [feat] add printf

--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "bin.h"
 
 int main(int ac, char *argv[])
 {
+    printf("main core here\n");
+
+    /*
+      Insert random Code here i don't want to do it ;-;
+    */
+
     return 0;
 }
